28be5cbcb81259604fe03385a97c10aea92352bf
Terry <saispas99@gmail.com>
2018-11-28 22:57:33 -0600

    Updated primary layout and buttons

--- app/src/main/java/com/example/tglinsey/chooseyouradventure/MainActivity.java
+++ app/src/main/java/com/example/tglinsey/chooseyouradventure/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         displayText = (TextView) findViewById(R.id.storyText);
         choice1 = (Button) findViewById(R.id.choice1);
